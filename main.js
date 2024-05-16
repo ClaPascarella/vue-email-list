44f67219c39a999b1b3769e4b23542149f1fc52a
ClaPascarella <claudiopascarella1996@gmail.com>
2024-05-16 11:35:13 +0200

    file css

--- main.js
+++ main.js
@@ -6,21 +6,35 @@ createApp({
         return {
             numEmail: 10,
             emails: [],
-            indirizzo: 'https://flynn.boolean.careers/exercises/api/random/mail'
+            indirizzo: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            
         }
     },
     methods: {
-        
+        getmail() {
+            
+            for(let i = 0; i < this.numEmail; i++){
+            axios.get(this.indirizzo).then((risultato) => {
+                    
+                    (this.emails).push(risultato.data.response);
+                   
+                });
+            }
+
+           
+            
+            
+        },
+
+         cancel(i) {
+                this.emails.splice(i, 10);
+
+            }
         
     },
     mounted() {
        
-        for(let i = 0; i < this.numEmail; i++){
-            axios.get(this.indirizzo).then((risultato) => {
-                
-                (this.emails).push(risultato.data.response);
-            });
-        }
+        
 
     }
 
